a0fce4c7b7a3590838999afe53dc3d07b68707c2
Joey <joey@cc.snxdao.io>
2024-05-27 12:01:35 +1000

    Make sure admin panel passes in network id

--- frontend/components/Admin/AdminPanel/index.js
+++ frontend/components/Admin/AdminPanel/index.js
@@ -1,28 +1,36 @@
-import { useState, useEffect } from 'react'
-import { Spinner } from '@chakra-ui/react'
-import Grants from './Grants'
-import TokenBalance from './TokenBalance'
-import RecentActivity from '../../shared/RecentActivity'
-import { useRecoilState } from 'recoil'
-import { getGrants, fetchGrants } from '../../../lib/store/grants'
-import { eventsState, fetchEvents } from '../../../lib/store/events'
+import { useState, useEffect } from "react";
+import { Spinner } from "@chakra-ui/react";
+import Grants from "./Grants";
+import TokenBalance from "./TokenBalance";
+import RecentActivity from "../../shared/RecentActivity";
+import { useRecoilState } from "recoil";
+import { getGrants, fetchGrants } from "../../../lib/store/grants";
+import { eventsState, fetchEvents } from "../../../lib/store/events";
 
 export default function AdminPanel() {
   const [grants, setGrant] = useRecoilState(getGrants());
   const [events, setEvents] = useRecoilState(eventsState);
   const [loadingData, setLoadingData] = useState(true);
+  const { chain } = useNetwork();
+  const networkId = chain.id;
 
   useEffect(() => {
-    Promise.all([fetchEvents(setEvents), fetchGrants(setGrant)]).finally(() => {
-      setLoadingData(false)
-    })
-  }, [])
+    if (!networkId) return;
+    Promise.all([
+      fetchEvents(setEvents, networkId),
+      fetchGrants(setGrant, networkId),
+    ]).finally(() => {
+      setLoadingData(false);
+    });
+  }, [networkId]);
 
-  return loadingData ? <Spinner d="block" mx="auto" my={6} /> : <>
-    <Grants />
-    <TokenBalance />
-    <RecentActivity />
-  </>
-
-
+  return loadingData ? (
+    <Spinner d="block" mx="auto" my={6} />
+  ) : (
+    <>
+      <Grants />
+      <TokenBalance />
+      <RecentActivity />
+    </>
+  );
 }
